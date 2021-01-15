55477ea8bde7647878be4a160b9b35108a429180
Nguyễn Trọng Thắng <61733031+gitthangnguyenAcE@users.noreply.github.com>
2021-01-15 19:59:53 +0700

    Update v4

---
+++ MyApp_ss4/MyApp_ss3/MyApp_ss3/Views/Admin.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyApp_ss3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Admin : ContentPage
+    {
+        public Admin()
+        {
+            InitializeComponent();
+        }
+            
+        private async void Aboutbutton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AdminTimKiem());
+        }
+        private async void AddListErrbutton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddListErr());
+        }
+        private async void Addpeoplebutton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddPerson());
+        }
+        private async void SearchListErrbutton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Search());
+        }
+        private async void SearchErrbutton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SearchXeVP());
+        }
+
+        private async void AddErrbutton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new AddXeVP());//want to go the previous page
+        }
+    }
+}
